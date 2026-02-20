9bb2fe83c3c2ee3381815596bc2995e62209b889
Dmitry Krylov <169095623+dxctor58@users.noreply.github.com>
2026-02-20 18:18:26 +0300

    test ci

--- app/app.js
+++ app/app.js
@@ -5,15 +5,17 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Docker CI/CD! New message');
+	res.send('Hello from Docker CI/CD! New message');
 });
 
 app.get('/add/:a/:b', (req, res) => {
-  const a = Number(req.params.a);
-  const b = Number(req.params.b);
-  res.send(`Result: ${add(a, b)}`);
+	const a = Number(req.params.a);
+	const b = Number(req.params.b);
+	res.send(`Result: ${add(a, b)}`);
 });
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on port ${port}`);
+	console.log(`Server running on port ${port}`);
 });
+
+console.log("test CI")
